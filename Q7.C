8b80a4717243530bbbc3f89640074e069179f3a5
AAYUSH-CODER16 <aayushgupta9636@gmail.com>
2026-09-09 19:01:35 +0530

    Add files via upload

---
+++ Q7.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a , b , c;
+    printf("a:");
+    scanf("%d",&a);
+    printf("b:");
+    scanf("%d",&b);
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
